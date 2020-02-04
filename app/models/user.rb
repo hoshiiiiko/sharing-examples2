bccbc54d9f549c89c7c23c8394c799d871883752
hoshiiiiko <57692673+hoshiiiiko@users.noreply.github.com>
2020-02-04 20:56:30 +0900

    ログイン画面を変えたい

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,22 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :authentication_keys => [:employee_number]
+  
+  validates_uniqueness_of :employee_number
+  validates_presence_of :employee_number
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      # 認証の条件式を変更する
+      where(conditions).where(["employee_number = :value", {:value => employee_number}]).first
+    else
+      where(conditions).first
+    end 
+  end
+
   has_many :posts
   has_many :likes, dependent: :destroy
   has_many :liked_posts, through: :likes, source: :post
