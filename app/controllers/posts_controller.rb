06f66326d11736669112ceaec3053fdbdc44be61
hoshiiiiko <57692673+hoshiiiiko@users.noreply.github.com>
2020-02-04 22:14:49 +0900

    create,newアクションを設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,16 @@ class PostsController < ApplicationController
   def show
   end
 
+  def new
+    @post = Post.new
+  end
+  
   def create
-    
+    Post.create(post_params)
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:image1, :image2, :image3, :point, :result, :appeal).merge(user_id: current_user.id, product_id: params[:product_id])
   end
 end
